7c3c3a2bd1a46112d69caa5aa328ee2c4ed6376f
vincent178 <vincent.007.cn@gmail.com>
2013-09-13 16:04:13 +0800

    new repo

---
+++ cb_asset_oss.gemspec
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+
+Gem::Specification.new do |s|
+  s.name = %q{cb_asset_oss}
+  #s.version = "0.3.0"
+  s.version = "0.4.#{Time.now.to_i}"
+
+  s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
+  s.authors = ["mangege"]
+  s.date = %q{2012-10-06}
+  s.description = %q{asset_oss is a library for uploading static assets to Aliyun OSS.}
+  s.email = %q{mr.mangege@gmail.com}
+  s.files = ["LICENSE", "README.md", "README_EN.textile","lib/asset_oss.rb"] + Dir.glob('lib/asset_oss/*.rb') + Dir.glob('lib/asset_oss/backend/*.rb')
+  s.has_rdoc = false
+  s.homepage = %q{http://github.com/mangege/asset_oss}
+  s.rdoc_options = ["--inline-source", "--charset=UTF-8"]
+  s.require_paths = ["lib"]
+  s.rubygems_version = %q{1.3.1}
+  s.summary = %q{asset_oss is a library for uploading static assets to Aliyun OSS.}
+
+  s.add_dependency("mime-types")
+  s.add_dependency("aliyun-oss")
+end
---
+++ lib/asset_oss/backend/oss.rb
@@ -0,0 +1,87 @@
+require 'aliyun/oss'
+
+module AssetOSS
+  class OSS
+  
+    def self.oss_config
+      @@config ||= YAML.load_file(File.join(Rails.root, "config/asset_oss.yml"))[Rails.env] rescue nil || {}
+    end
+  
+    def self.connect_to_oss
+      Aliyun::OSS::Base.establish_connection!(
+        :server => oss_config['host'] || Aliyun::OSS::DEFAULT_HOST,
+        :access_key_id => oss_config['access_key_id'],
+        :secret_access_key => oss_config['secret_access_key']
+      )
+    end
+  
+    def self.oss_permissions
+      :public_read
+    end
+  
+    def self.oss_bucket
+      oss_config['bucket']
+    end
+    
+    def self.oss_folder
+      oss_config['folder']
+    end
+    
+    def self.oss_prefix
+      oss_config['prefix'] || oss_bucket_url
+    end
+    
+    def self.oss_bucket_url
+      "http://#{oss_bucket}.oss.aliyuncs.com#{oss_folder ? "/#{oss_folder}" : '' }"
+    end
+    
+    def self.full_path(asset)
+      oss_folder ? "/#{oss_folder}#{asset.fingerprint}" : asset.fingerprint
+    end
+    
+    def self.upload(options={})
+      Asset.init(:debug => options[:debug], :nofingerprint => options[:nofingerprint])
+      
+      assets = Asset.find
+      return if assets.empty?
+    
+      connect_to_oss
+
+      Aliyun::OSS::Bucket.create(oss_bucket, :access => oss_permissions)
+    
+      assets.each do |asset|
+      
+        puts "AssetOSS: #{asset.relative_path}" if options[:debug]
+      
+        headers = {
+          :content_type => asset.mime_type,
+        }.merge(asset.cache_headers)
+        
+        asset.replace_css_images!(:prefix => oss_prefix) if asset.css?
+        
+        if asset.gzip_type?
+          headers.merge!(asset.gzip_headers)
+          asset.gzip!
+        end
+        
+        if options[:debug]
+          puts "  - Uploading: #{full_path(asset)} [#{asset.data.size} bytes]"
+          puts "  - Headers: #{headers.inspect}"
+        end
+        
+        unless options[:dry_run]
+          res = Aliyun::OSS::OSSObject.store(
+            full_path(asset),
+            asset.data,
+            oss_bucket,
+            headers
+          ) 
+          puts "  - Response: #{res.inspect}" if options[:debug]
+        end
+      end
+    
+      Cache.save! unless options[:dry_run]
+    end
+  
+  end
+end
